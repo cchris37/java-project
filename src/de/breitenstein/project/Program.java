009c0e9d49a5aa19b801de7432d08c8ffaec1b69
cchris37 <57228138+cchris37@users.noreply.github.com>
2020-02-20 21:30:18 +0100

    * installed JDk * installed Intellij IDEA * Initial Commit

---
+++ src/de/breitenstein/project/Program.java
@@ -0,0 +1,24 @@
+package de.breitenstein.project;
+
+public class Program {
+
+    public static void main(String[] args) {
+        /*System.out.println("Hello World");
+
+		int age = 27;	//Initialisierung
+
+		int age2; 		//Deklaration
+		//age2 = 29;		//Zuweisung
+
+		System.out.println(age);
+		age2 = 30;
+		age = 28;
+		System.out.println(age);
+		System.out.println(age2);*/
+
+        int test = 128;
+        System.out.println(test);
+
+    }
+}
+
