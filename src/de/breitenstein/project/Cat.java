f6d599e95f3dfbc11922696ed6b89d2999880a0e
cchris37 <57228138+cchris37@users.noreply.github.com>
2020-03-11 16:52:42 +0100

    Vererbung

--- src/de/breitenstein/project/Cat.java
+++ src/de/breitenstein/project/Cat.java
@@ -1,6 +1,6 @@
 package de.breitenstein.project;
 
-public class Cat extends Animal { //Vererbung
+public class Cat extends Animal { //Vererbung //Erbt von Animal
 
     public void purr() {
         System.out.println("nya nya");
