80d906b2940208722a40b3445759e58adf6b9f75
cchris37 <57228138+cchris37@users.noreply.github.com>
2020-03-11 16:54:10 +0100

    Vererbung

--- src/de/breitenstein/project/Dog.java
+++ src/de/breitenstein/project/Dog.java
@@ -1,6 +1,6 @@
 package de.breitenstein.project;
 
-public class Dog extends Animal { //Vererbung //Erbt von Animal
+public class Dog extends Animal { //Vererbung //Erbt von Animal.
 
     public void bark() {
         System.out.println("wau wau");
